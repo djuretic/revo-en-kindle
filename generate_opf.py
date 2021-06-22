4792d1d0cdf5f1b155993263adc89e619710479c
Dušan Juretić <dusan.juretic@gmail.com>
2021-06-21 22:07:15 -0400

    Generate .opf

---
+++ generate_opf.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+def main():
+    with open('template.opf') as f:
+        lines = f.readlines()
+    
+    html_dir = Path('./output')
+    html_files = html_dir.glob("*.html")
+
+    n = 0
+
+    with open("./output/vortaro.opf", "w") as f:
+        for line in lines:
+            if "<item id" in line:
+                for n, html in enumerate(html_files):
+                    f.write(f"""    <item id="dictionary{n}" href="{html.name}" media-type="text/x-oeb1-document"/>\n""")
+            elif "<itemref" in line:
+                # the previous loop will set "n"
+                for n in range(n + 1):
+                    f.write(f"""         <itemref idref="dictionary{n}"/>\n""")
+            else:
+                f.write(line)
+
+
+if __name__ == "__main__":
+    main()
